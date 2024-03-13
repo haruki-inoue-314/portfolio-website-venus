4958aad1fef96337924994f30e8e8ff00230ab19
HarukiInoue <aitk15015kk@outlook.com>
2024-03-14 01:50:58 +0900

    add: プロフィールを実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,20 @@ export default function Home() {
   return (
     <div className="container mx-auto px-4">
       <main>
-        <div className="relative w-full">
-          <div className="flex justify-center">
-            <h1 className="text-4xl p-2">3.14 Laboratory</h1>
+        <div className="flex flex-col items-center">
+          <div className="mt-8 mb-2 w-[96px] h-[96px]">
+            <img
+              src="identity_icon.png"
+              alt="さんてんいちよんのアイデンティティアイコン"
+              className="w-auto h-auto border rounded-full shadow"
+            />
           </div>
+          <h1 className="mt-2 mb-1 text-2xl font-bold">さんてんいちよん</h1>
+          <p className="mt-1 mb-4">
+            iOSアプリエンジニア ときどき Webフロントエンジニア
+          </p>
         </div>
+
         <SelfIntroduction />
       </main>
     </div>
--- src/components/SelfIntroduction.tsx
+++ src/components/SelfIntroduction.tsx
@@ -1,42 +1,106 @@
+import Link from "next/link";
+
 export default function SelfIntroduction() {
-  const profiles: [
+  type Profile = {
+    title: string;
+    paragraph: string;
+  };
+
+  type Skill = {
+    title: string;
+    bgColor: string;
+    fontCcolor: string;
+  };
+
+  type Account = {
+    title: string;
+    to: string;
+    bgColor: string;
+    fontCcolor: string;
+  };
+
+  const profiles: Profile[] = [
     {
-      title: string;
-      paragraph: string;
-    }
-  ] = [
+      title: "すまい",
+      paragraph: "愛知県豊橋市 → 千葉県流山市",
+    },
+  ];
+
+  const skillset: Skill[] = [
+    { title: "Swift", bgColor: "bg-orange-500", fontCcolor: "text-white" },
+    { title: "SwiftUI", bgColor: "bg-orange-500", fontCcolor: "text-white" },
+    { title: "UIKit", bgColor: "bg-orange-500", fontCcolor: "text-white" },
+    { title: "TypeScript", bgColor: "bg-blue-500", fontCcolor: "text-white" },
+    { title: "Vue.js", bgColor: "bg-green-500", fontCcolor: "text-white" },
+    { title: "React", bgColor: "bg-slate-700", fontCcolor: "text-white" },
+    { title: "Kotlin", bgColor: "bg-orange-500", fontCcolor: "text-white" },
+    { title: "Android", bgColor: "bg-green-500", fontCcolor: "text-white" },
+  ];
+
+  const acounts: Account[] = [
     {
-      title: "居住地",
-      paragraph: "千葉県流山市",
+      title: "Twitter",
+      to: "https://twitter.com/kyotonagoya1476",
+      bgColor: "bg-[#1DA1F2]",
+      fontCcolor: "text-white",
     },
     {
-      title: "出身",
-      paragraph: "愛知県豊橋市",
+      title: "Qiita",
+      to: "https://qiita.com/kyotonagoya",
+      bgColor: "bg-[#55c550]",
+      fontCcolor: "text-white",
     },
     {
-      title: "職業",
-      paragraph: "iOSアプリエンジニア 時々 Webフロントエンジニア",
+      title: "GitHub",
+      to: "https://github.com/k15015kk",
+      bgColor: "bg-[#171515]",
+      fontCcolor: "text-white",
+    },
+    {
+      title: "Wantedly",
+      to: "https://www.wantedly.com/id/kyotonagoya",
+      bgColor: "bg-[#f0f0f0]",
+      fontCcolor: "text-black",
     },
   ];
 
   return (
-    <div className="max-w-[640px] m-auto bg-gray-50 shadow rounded p-4">
-      <div className="flex">
-        <div className="py-2 px-4 basis-24">
-          <img
-            src="identity_icon.png"
-            alt="さんてんいちよんのアイデンティティアイコン"
-            className="w-auto h-auto border rounded-full shadow"
-          />
+    <div className="max-w-[720px] p-4 m-auto bg-gray-100 rounded-xl">
+      <div className="mx-4">
+        {profiles.map((content) => (
+          <div className="my-2" key={content.title}>
+            <h3 className="my-1 text-gray-900 font-bold">{content.title}</h3>
+            <p className="text-gray-900">{content.paragraph}</p>
+          </div>
+        ))}
+
+        <div className="my-2">
+          <h3 className="my-1 text-gray-900 font-bold">スキル</h3>
+          <div className="flex flex-wrap">
+            {skillset.map((skill) => (
+              <span
+                className={`block ${skill.bgColor} ${skill.fontCcolor} py-1 px-2 m-1 text-sm rounded`}
+                key={skill.title}
+              >
+                {skill.title}
+              </span>
+            ))}
+          </div>
         </div>
-        <div>
-          <h2 className="text-2xl font-bold pt-2 pb-4">さんてんいちよん</h2>
-          {profiles.map((content) => (
-            <div className="flex py-1 px-2" key={content.title}>
-              <h3 className="font-bold w-16 pe-2">{content.title}</h3>
-              <p>{content.paragraph}</p>
-            </div>
-          ))}
+
+        <div className="my-2">
+          <h3 className="my-1 text-gray-900 font-bold">アカウント</h3>
+          <div className="flex my-1 flex-wrap">
+            {acounts.map((acount) => (
+              <Link
+                href={acount.to}
+                className={`${acount.bgColor} ${acount.fontCcolor} mx-1 my-1 px-3 py-1 font-bold shadow rounded hover:opacity-50`}
+                key={acount.title}
+              >
+                {acount.title}
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </div>
