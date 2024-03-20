75fb90772893426734558c4a781820587e45d9b2
HarukiInoue <aitk15015kk@outlook.com>
2024-03-20 13:05:24 +0900

    fix: アカウントの設置場所を変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,34 @@
 import SelfIntroduction from "@/components/SelfIntroduction";
+import Link from "next/link";
 
 export default function Home() {
+  const acounts: Account[] = [
+    {
+      title: "Twitter",
+      to: "https://twitter.com/kyotonagoya1476",
+      bgColor: "bg-[#1DA1F2]",
+      fontCcolor: "text-white",
+    },
+    {
+      title: "Qiita",
+      to: "https://qiita.com/kyotonagoya",
+      bgColor: "bg-[#55c550]",
+      fontCcolor: "text-white",
+    },
+    {
+      title: "GitHub",
+      to: "https://github.com/k15015kk",
+      bgColor: "bg-[#171515]",
+      fontCcolor: "text-white",
+    },
+    {
+      title: "Wantedly",
+      to: "https://www.wantedly.com/id/kyotonagoya",
+      bgColor: "bg-[#f0f0f0]",
+      fontCcolor: "text-black",
+    },
+  ];
+
   return (
     <div className="container mx-auto px-4">
       <main>
@@ -13,9 +41,22 @@ export default function Home() {
             />
           </div>
           <h1 className="mt-2 mb-1 text-2xl font-bold">さんてんいちよん</h1>
-          <p className="mt-1 mb-4">
+          <p className="my-2">
             iOSアプリエンジニア ときどき Webフロントエンジニア
           </p>
+          <div className="my-2">
+            <div className="flex flex-wrap">
+              {acounts.map((acount) => (
+                <Link
+                  href={acount.to}
+                  className={`${acount.bgColor} ${acount.fontCcolor} mx-1 my-1 px-3 py-1 font-bold shadow rounded hover:opacity-50`}
+                  key={acount.title}
+                >
+                  {acount.title}
+                </Link>
+              ))}
+            </div>
+          </div>
         </div>
 
         <SelfIntroduction />
--- src/components/SelfIntroduction.tsx
+++ src/components/SelfIntroduction.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 export default function SelfIntroduction() {
   type Profile = {
     title: string;
@@ -19,60 +17,24 @@ export default function SelfIntroduction() {
     fontCcolor: string;
   };
 
-  const profiles: Profile[] = [
-    {
-      title: "すまい",
-      paragraph: "愛知県豊橋市 → 千葉県流山市",
-    },
-  ];
-
   const skillset: Skill[] = [
-    { title: "Swift", bgColor: "bg-orange-500", fontCcolor: "text-white" },
-    { title: "SwiftUI", bgColor: "bg-orange-500", fontCcolor: "text-white" },
-    { title: "UIKit", bgColor: "bg-orange-500", fontCcolor: "text-white" },
-    { title: "TypeScript", bgColor: "bg-blue-500", fontCcolor: "text-white" },
-    { title: "Vue.js", bgColor: "bg-green-500", fontCcolor: "text-white" },
-    { title: "React", bgColor: "bg-slate-700", fontCcolor: "text-white" },
-    { title: "Kotlin", bgColor: "bg-orange-500", fontCcolor: "text-white" },
-    { title: "Android", bgColor: "bg-green-500", fontCcolor: "text-white" },
-  ];
-
-  const acounts: Account[] = [
-    {
-      title: "Twitter",
-      to: "https://twitter.com/kyotonagoya1476",
-      bgColor: "bg-[#1DA1F2]",
-      fontCcolor: "text-white",
-    },
-    {
-      title: "Qiita",
-      to: "https://qiita.com/kyotonagoya",
-      bgColor: "bg-[#55c550]",
-      fontCcolor: "text-white",
-    },
-    {
-      title: "GitHub",
-      to: "https://github.com/k15015kk",
-      bgColor: "bg-[#171515]",
-      fontCcolor: "text-white",
-    },
-    {
-      title: "Wantedly",
-      to: "https://www.wantedly.com/id/kyotonagoya",
-      bgColor: "bg-[#f0f0f0]",
-      fontCcolor: "text-black",
-    },
+    { title: "Swift", bgColor: "bg-amber-800", fontCcolor: "text-white" },
+    { title: "SwiftUI", bgColor: "bg-amber-800", fontCcolor: "text-white" },
+    { title: "UIKit", bgColor: "bg-amber-800", fontCcolor: "text-white" },
+    { title: "TypeScript", bgColor: "bg-slate-800", fontCcolor: "text-white" },
+    { title: "Vue.js", bgColor: "bg-slate-800", fontCcolor: "text-white" },
+    { title: "React", bgColor: "bg-slate-800", fontCcolor: "text-white" },
+    { title: "Kotlin", bgColor: "bg-slate-800", fontCcolor: "text-white" },
+    { title: "Android", bgColor: "bg-slate-800", fontCcolor: "text-white" },
   ];
 
   return (
     <div className="max-w-[720px] p-4 m-auto bg-gray-100 rounded-xl">
       <div className="mx-4">
-        {profiles.map((content) => (
-          <div className="my-2" key={content.title}>
-            <h3 className="my-1 text-gray-900 font-bold">{content.title}</h3>
-            <p className="text-gray-900">{content.paragraph}</p>
-          </div>
-        ))}
+        <div className="my-2">
+          <h3 className="my-1 text-gray-900 font-bold">すまい</h3>
+          <p className="text-gray-900">愛知県豊橋市→千葉県流山市</p>
+        </div>
 
         <div className="my-2">
           <h3 className="my-1 text-gray-900 font-bold">スキル</h3>
@@ -84,21 +46,6 @@ export default function SelfIntroduction() {
               >
                 {skill.title}
               </span>
-            ))}
-          </div>
-        </div>
-
-        <div className="my-2">
-          <h3 className="my-1 text-gray-900 font-bold">アカウント</h3>
-          <div className="flex my-1 flex-wrap">
-            {acounts.map((acount) => (
-              <Link
-                href={acount.to}
-                className={`${acount.bgColor} ${acount.fontCcolor} mx-1 my-1 px-3 py-1 font-bold shadow rounded hover:opacity-50`}
-                key={acount.title}
-              >
-                {acount.title}
-              </Link>
             ))}
           </div>
         </div>
